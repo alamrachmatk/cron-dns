27ea9a78105da3647cba63909d0ca9f852214678
alam.kurniawan <alam.kurniawan@mncgroup.com>
2020-12-14 23:14:02 +0700

    handle filepath

--- main.go
+++ main.go
@@ -20,15 +20,13 @@ func main() {
 	// Initialize main database
 	db.Db = db.MariaDBInit()
 
-	gocron.Every(1).Seconds().Do(LogDns)
+	gocron.Every(5).Seconds().Do(LogDns)
 
 	<-gocron.Start()
 }
 
 func LogDns() {
-
-	log.Println("TEST DNS FILTER")
-	return
+	log.Println("RUNNING CRON DNS")
 	var files []string
 	root := config.DnsLog
 	err := filepath.Walk(root, visit(&files))
@@ -37,81 +35,88 @@ func LogDns() {
 	} else {
 		for a, getFile := range files {
 			if a != 0 {
-				log.Println("base: ", filepath.Base(getFile))
-				file, err := os.Open(config.DnsLog + filepath.Base(getFile)) // For read access.
-				partsFilename := strings.Split(file.Name(), "_")
-				partsDate := strings.Split(partsFilename[2], "-")
-				//get date
-				date := partsDate[2] + "-" + partsDate[1] + "-" + partsDate[0]
-				checkQuestion := "question for '"
-				if err != nil {
-					log.Fatal(err)
-				}
-				scanner := bufio.NewScanner(file)
-				scanner.Split(bufio.ScanLines)
-				var txtlines []string
+				fileName := config.DnsLog + filepath.Base(getFile)
+				var extension = filepath.Ext(fileName)
+				if extension != ".filepart" {
+					file, err := os.Open(fileName) // For read access.
+					partsFilename := strings.Split(file.Name(), "_")
+					partsDate := strings.Split(partsFilename[2], "-")
+					//get date
+					date := partsDate[2] + "-" + partsDate[1] + "-" + partsDate[0]
+					checkQuestion := "question for '"
+					if err != nil {
+						log.Println(err)
+					}
+					scanner := bufio.NewScanner(file)
+					scanner.Split(bufio.ScanLines)
+					var txtlines []string
 
-				for scanner.Scan() {
-					txtlines = append(txtlines, scanner.Text())
-				}
+					for scanner.Scan() {
+						txtlines = append(txtlines, scanner.Text())
+					}
 
-				file.Close()
+					file.Close()
 
-				var ipAddress string
-				var domain string
-				var baseDomain string
-				var hasSubDomain string
-				var time string
-				var dateTime string
-				params := make(map[string]string)
-				for _, eachline := range txtlines {
-					if strings.Contains(eachline, checkQuestion) {
-						splitTime := strings.Split(eachline, " ")
-						time = splitTime[3]
-						result := strings.SplitAfter(eachline, checkQuestion)
-						for i := range result {
-							if i == 1 {
-								dateTime = date + " " + time
-								var partDomain, partAfterDomain string
-								if i := strings.Index(result[1], ".|"); i >= 0 {
-									partDomain, partAfterDomain = result[1][:i], result[1][i:]
-								}
-								partIP := strings.Split(partAfterDomain, "from ")
-								rep := partDomain + "|" + partIP[1]
-								parts := strings.Split(rep, "|")
-								// get ip addreess & domain & basedomain
-								for i := range parts {
-									if i == 0 {
-										domain = parts[0]
-										baseDomain = domainutil.Domain(domain)
-										if baseDomain == "" {
-											baseDomain = domain
+					var ipAddress string
+					var domain string
+					var baseDomain string
+					var hasSubDomain string
+					var time string
+					var dateTime string
+					params := make(map[string]string)
+					for _, eachline := range txtlines {
+						if strings.Contains(eachline, checkQuestion) {
+							splitTime := strings.Split(eachline, " ")
+							time = splitTime[2]
+							result := strings.SplitAfter(eachline, checkQuestion)
+							for i := range result {
+								if i == 1 {
+									dateTime = date + " " + time
+									log.Println("TIME CUY ", time)
+									var partDomain, partAfterDomain string
+									if i := strings.Index(result[1], ".|"); i >= 0 {
+										partDomain, partAfterDomain = result[1][:i], result[1][i:]
+									}
+									partIP := strings.Split(partAfterDomain, "from ")
+									rep := partDomain + "|" + partIP[1]
+									parts := strings.Split(rep, "|")
+									// get ip addreess & domain & basedomain
+									for i := range parts {
+										if i == 0 {
+											domain = parts[0]
+											baseDomain = domainutil.Domain(domain)
+											if baseDomain == "" {
+												baseDomain = domain
+											}
+											if domainutil.HasSubdomain(domain) == true {
+												hasSubDomain = "1"
+											} else {
+												hasSubDomain = "0"
+											}
 										}
-										if domainutil.HasSubdomain(domain) == true {
-											hasSubDomain = "1"
-										} else {
-											hasSubDomain = "0"
+										if i == 0 {
+											ipAddress = parts[1]
 										}
 									}
-									if i == 0 {
-										ipAddress = parts[1]
+									params["domain"] = domain
+									params["base_domain"] = baseDomain
+									params["ip_address"] = ipAddress
+									params["has_subdomain"] = hasSubDomain
+									params["log_datetime"] = dateTime
+									statusResponse, err := models.CreateDns(params)
+									if statusResponse != 200 {
+										log.Println(err)
 									}
-								}
-								params["domain"] = domain
-								params["base_domain"] = baseDomain
-								params["ip_address"] = ipAddress
-								params["has_subdomain"] = hasSubDomain
-								params["log_datetime"] = dateTime
-								statusResponse, err := models.CreateDns(params)
-								if statusResponse != 200 {
-									log.Println(err)
 								}
 							}
-						}
 
+						}
+					}
+					e := os.Remove(fileName)
+					if e != nil {
+						log.Println(e)
 					}
 				}
-				return
 			}
 		}
 	}
@@ -120,7 +125,7 @@ func LogDns() {
 func visit(files *[]string) filepath.WalkFunc {
 	return func(path string, info os.FileInfo, err error) error {
 		if err != nil {
-			log.Fatal(err)
+			log.Println(err)
 		}
 		*files = append(*files, path)
 		return nil
